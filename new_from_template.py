9c0ed2ddc63c79a094f8bec0f859f026bef712e6
Alex McLain <alexm@morgansound.com>
2013-08-20 14:35:46 -0700

    Script has to load NetlinxTemplate class first.

--- new_from_template.py
+++ new_from_template.py
@@ -1,15 +1,5 @@
 import sublime, sublime_plugin, os
 
-# NetLinx Standard Template
-class NetlinxNewFromTemplateStandard(sublime_plugin.TextCommand, NetlinxTemplate):
-    def run(self, edit):
-        self.load_template(edit, 'Netlinx Standard.axs')
-        
-# NetLinx Test Suite Template
-class NetlinxNewFromTemplateTestSuite(sublime_plugin.TextCommand, NetlinxTemplate):
-    def run(self, edit):
-        self.load_template(edit, 'Netlinx Test Suite.axs')
-
 class NetlinxTemplate:
     def load_template(self, edit, filename):
         # Read in the template.
@@ -21,3 +11,13 @@ class NetlinxTemplate:
         v = self.view.window().new_file()
         v.insert(edit, 0, content)
         v.set_syntax_file('Packages/NetLinx/NetLinx.tmLanguage')
+
+# NetLinx Standard Template
+class NetlinxNewFromTemplateStandard(sublime_plugin.TextCommand, NetlinxTemplate):
+    def run(self, edit):
+        self.load_template(edit, 'Netlinx Standard.axs')
+        
+# NetLinx Test Suite Template
+class NetlinxNewFromTemplateTestSuite(sublime_plugin.TextCommand, NetlinxTemplate):
+    def run(self, edit):
+        self.load_template(edit, 'Netlinx Test Suite.axs')
