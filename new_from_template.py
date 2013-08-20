821a41a20590962c084d43486b292d9a5e2123cc
Alex McLain <alexm@morgansound.com>
2013-08-20 14:26:47 -0700

    Refactored template script to allow for easier expansion of additional templates.

--- new_from_template.py
+++ new_from_template.py
@@ -1,27 +1,19 @@
 import sublime, sublime_plugin, os
 
 # NetLinx Standard Template
-class NetlinxNewFromTemplateStandard(sublime_plugin.TextCommand):
+class NetlinxNewFromTemplateStandard(sublime_plugin.TextCommand, NetlinxTemplate):
     def run(self, edit):
-        template_name = 'Netlinx Standard.axs'
+        self.load_template(edit, 'Netlinx Standard.axs')
         
-        # Read in the template.
-        f = open(os.path.join(os.path.dirname(__file__), 'templates', template_name))
-        content = f.read()
-        f.close()
-        
-        # Create a new view (tab) and fill it with the template text.
-        v = self.view.window().new_file()
-        v.insert(edit, 0, content)
-        v.set_syntax_file('Packages/NetLinx/NetLinx.tmLanguage')
-
 # NetLinx Test Suite Template
-class NetlinxNewFromTemplateTestSuite(sublime_plugin.TextCommand):
+class NetlinxNewFromTemplateTestSuite(sublime_plugin.TextCommand, NetlinxTemplate):
     def run(self, edit):
-        template_name = 'Netlinx Test Suite.axs'
-        
+        self.load_template(edit, 'Netlinx Test Suite.axs')
+
+class NetlinxTemplate:
+    def load_template(self, edit, filename):
         # Read in the template.
-        f = open(os.path.join(os.path.dirname(__file__), 'templates', template_name))
+        f = open(os.path.join(os.path.dirname(__file__), 'templates', filename))
         content = f.read()
         f.close()
         
